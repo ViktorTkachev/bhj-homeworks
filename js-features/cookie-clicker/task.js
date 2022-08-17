9d1326e5decff6743547fd618043aafa12cd49b3
Viktor <mr.viktor.tkachev@gmail.com>
2022-08-17 20:40:02 +0300

    meke the task cookie cliker and add some new image, co

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,11 @@
+let clickerCounter = document.getElementById('clicker__counter');
+let image = document.getElementById('cookie');
+
+image.onclick = function() {
+  clickerCounter.textContent++;
+  if (clickerCounter.textContent % 2 == 0) {
+    image.width = 200;
+  } else {
+    image.width = 250;
+  }
+}

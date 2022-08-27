c1a497d5713a9968b9c0345666eb9fdb3a6f48c7
Viktor <mr.viktor.tkachev@gmail.com>
2022-08-27 18:13:44 +0300

    add changes

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,21 +2,20 @@
   let counterDead = document.getElementById('dead');
   let counterLost = document.getElementById('lost');
 
-  const hole = index => document.getElementById(`hole${index}`),
-  checkHole = index => hole(index).onclick = function() {
-    if (hole(index).classList.contains('hole_has-mole')) {
-      counterDead.textContent++;
-    } else {
-      counterLost.textContent++;
-    }
-
-    if (counterLost == 5) {
+  function checkWinOrLost () {
+    if (counterLost.textContent == 5) {
       alert('Вы проиграли!');
-      counterLost.textContent = 0;
-      counterDead.textContent = 0;
-    } else if (counterDead == 10) {
+      counterLost.textContent, counterDead.textContent = 0;
+    } else if (counterDead.textContent == 10) {
       alert('Вы победили!');
-      counterLost.textContent = 0;
-      counterDead.textContent = 0;
+      counterLost.textContent = 0, counterDead.textContent = 0;
+    }
+  }
+
+  const hole = index => document.getElementById(`hole${index}`)
+  for (let i = 1; i < 10; i++) {
+    hole(i).onclick = () => {
+      hole(i).classList.contains('hole_has-mole') ? counterDead.textContent++ : counterLost.textContent++;
+      checkWinOrLost()
     }
   }

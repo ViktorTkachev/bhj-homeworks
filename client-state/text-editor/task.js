d6e873b4d07561133d9f6f816cf67d1c75fb4b4c
Viktor <mr.viktor.tkachev@gmail.com>
2023-01-22 02:25:58 +0800

    make text-editor of client-state

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,11 @@
+const getTextAria = document.getElementById('editor');
+const storedTextAriaValue = localStorage.getItem('textAriaText')
+
+if (storedTextAriaValue) {
+  getTextAria.value = storedTextAriaValue;
+}
+
+getTextAria.addEventListener('input', () => {
+  const textAriaValue = getTextAria.value;
+  localStorage.setItem('textAriaText', textAriaValue);
+})
